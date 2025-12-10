ab8714d31a956b2401b308c85b6e68f7b0c6b95d
EdgarCavalinhos <edgarcavalinhos@hotmail.com>
2025-12-10 20:15:55 +0000

    Fix api url

--- frontend/src/hooks/useArticles.js
+++ frontend/src/hooks/useArticles.js
@@ -5,7 +5,7 @@ export function useArticles() {
     const [loading, setLoading] = useState(true); 
 
     useEffect(() => {
-        fetch('http://localhost:3000/get-articles')
+        fetch('http://54.170.63.117:3000/get-articles')
             .then(response => response.json())
             .then(data => {
                 const sortedData = data.sort((a, b) => b.id - a.id);
